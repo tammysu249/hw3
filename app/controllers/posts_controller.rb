becd6cb185df77788ad5a52a6b7270cf6db84bd0
tammysu249 <tammy.su@kellogg.northwestern.edu>
2022-02-25 22:53:50 +0000

    posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+
+    def new
+        @post = Post.new
+        @place = Place.find(params["name"])
+        @post.name = @name
+    end
+
+    def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places/#{@post.name}"
+    end
+
 end
