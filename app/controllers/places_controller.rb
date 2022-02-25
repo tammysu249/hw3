de99713276482cd32aec1cddc74a196e5e258ca6
tammysu249 <tammy.su@kellogg.northwestern.edu>
2022-02-25 22:36:34 +0000

    places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,17 @@
 class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+    end
+
 end
